70939ff043763f9b963ffb891afce1d8c82eba12
MoosaAfzal <88625853+MoosaAfzal@users.noreply.github.com>
2023-09-09 03:30:48 +0500

    Update page.tsx

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -62,7 +62,7 @@ const Blog = () => {
                         </div>
                     </div>
                     <h1 className='mt-4'>Smarter Ways to Manage EV Charging Infrastructures</h1>
-                    <div className='max-sm:space-y-4'>
+                    <div className='space-y-4'>
                         <p>Quuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quia non numquam eius modi tempora incidunt ut labore et dolore magnam dolor sit amet, consectetur adipisicing.</p>
                         <p>Quuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quia non numquam eius modi tempora incidunt ut labore et dolore magnam dolor sit amet, consectetur adipisicing.</p>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
